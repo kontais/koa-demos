debd4365ddebd843764d7b00041736da17f82b3e
kontais <kontais@aliyun.com>
2017-12-24 17:26:32 +0800

    500, 400

--- demos/06.js
+++ demos/06.js
@@ -2,16 +2,54 @@ const Koa = require('koa');
 const route = require('koa-route');
 const app = new Koa();
 
-const about = ctx => {
-  ctx.response.type = 'html';
-  ctx.response.body = '<a href="/">Index Page</a>';
+// 最外层的中间件，负责所有中间件的错误处理
+// 重构500
+const error_handler = async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.response.status = err.statusCode || err.status || 500;
+    ctx.response.body = {
+      message: err.message
+    };
+    ctx.app.emit('error', err, ctx);
+  }
 };
+app.use(error_handler);
+
+// 运行过程中一旦出错，Koa 会触发一个error事件。监听这个事件，也可以处理错误。
+app.on('error', (err, ctx) => {
+  console.error('server error:', err.message);
+});
 
 const main = ctx => {
   ctx.response.body = 'Hello World';
 };
 
+const about = ctx => {
+  ctx.response.type = 'html';
+  ctx.response.body = '<a href="/">Index Page</a>';
+};
 app.use(route.get('/', main));
 app.use(route.get('/about', about));
 
+const throw_error = ctx => {
+  ctx.throw(500);
+};
+app.use(route.get('/error', throw_error));
+
+const app_error = ctx => {
+  throw new Error('app error\n');
+};
+app.use(route.get('/app_error', app_error));
+
+// 重构404
+const not_found = ctx => {
+  ctx.response.status = 404;
+  ctx.response.body = 'Page Not Found';
+};
+app.use(not_found);
+
 app.listen(3000);
+
+
--- demos/12.js
+++ demos/12.js
@@ -3,7 +3,7 @@ const app = new Koa();
 const path = require('path');
 const serve = require('koa-static');
 
-const main = serve(path.join(__dirname));
+const main = serve(path.join(__dirname, 'public'));
 
 app.use(main);
 app.listen(3000);
